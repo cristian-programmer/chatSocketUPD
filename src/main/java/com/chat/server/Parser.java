2f421ebf790bd50c2ace18d06b9d1df3e9145090
Cristian Vargas <cvg97@misena.edu.co>
2020-06-14 10:12:29 -0500

    se añadio un Parser de strings, para separar la accion que se quiere que haga el servidor y la data o el mensaje que llega

---
+++ src/main/java/com/chat/server/Parser.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chat.server;
+
+/**
+ *
+ * @author User
+ */
+public class Parser {
+    final static String SYMBOL = ":::";
+    final static String ACTION_ADD_USER = "addUser";
+    final static String ACTION_SEND_MESSAGE = "form";
+    final static String GET_ALL_USERS="getUsers";
+
+    public String message; 
+    public Parser(String message) {
+       this.message =  message;
+    }
+    public String[] breakMessage(){
+       return this.message.split(SYMBOL);
+    }
+    
+}
